0170bb4bc67d7cc2a2cd839695a2210b2ca55497
andrebianco-net <abianco.allegro@gmail.com>
2023-11-16 17:06:02 -0300

    Add Clean Arch structure

---
+++ SalesOrderSenderService/SalesOrderSenderService.Worker/Program.cs
@@ -0,0 +1,10 @@
+using SalesOrderSenderService.Worker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
